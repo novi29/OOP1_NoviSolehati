928b44d00afb2baa9b33de31867635554f138fba
novi29 <novisolehati@yahoo.com>
2016-03-29 21:05:36 +0700

    Added files via upload

---
+++ ContohMethod.java
@@ -0,0 +1,23 @@
+public class ContohMethod {
+
+public static void main (String[] args ){
+
+ContohMethod nama = new ContohMethod();
+nama.namaSaya();
+System.out.println(nama.namaSaya());
+nama.pertambahan();
+}
+
+public String namaSaya(){
+return "Novi";
+}
+public void pertambahan() {
+int a = 1;
+int b = 2;
+System.out.println(a+b);
+
+}
+
+
+
+}
