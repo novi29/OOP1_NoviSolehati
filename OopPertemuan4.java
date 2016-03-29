c604691b554c285eeeda4c0b761f4398c5505fe7
novi29 <novisolehati@yahoo.com>
2016-03-29 21:06:28 +0700

    Added files via upload

---
+++ OopPertemuan4.java
@@ -0,0 +1,26 @@
+package ooppertemuan4;
+
+public class OopPertemuan4 {
+
+
+    public static void main(String[] args) {
+        OopPertemuan4 belajar = new OopPertemuan4();
+        belajar.tambah(1, 2);
+        belajar.kurang (5, 3);
+    }
+        private void tambah (int a, int b) {
+            int hasil;
+            hasil = a + b;
+            System.out.println(hasil);
+        }
+        private void kurang (int a, int b) {
+            int hasil;
+            hasil = a - b;
+            System.out.println(hasil);
+            
+        }
+        public static void main(String[] args) {
+            
+    }
+    
+}
