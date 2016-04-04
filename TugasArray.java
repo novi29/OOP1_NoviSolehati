1ea83ed77b9e173046562ae98f821aba6d059979
novi29 <novisolehati@yahoo.com>
2016-04-04 14:39:03 +0700

    Added files via upload

---
+++ TugasArray.java
@@ -0,0 +1,32 @@
+package tugasarray;
+
+public class TugasArray {
+
+    public static void main(String[] args) {
+int a =10;
+ String [] b = new String [10];
+  b[0]= "PERALATAN SEKOLAH ";
+    b[1]= "Buku Tulis";
+    b[2]= "Pensil";
+    b[3]= "Pulpen";
+    b[4]= "Penggaris";
+    b[5]= "Busur";
+    b[6]= "Tipex";
+    b[7]= "Kotak Pensil";
+    b[8]= "Buku Gambar";
+    b[9]= "Pensil Gambar";
+    
+    System.out.println(b [0]);
+        System.out.println("1 " + b [1]);
+        System.out.println("2 " + b [2]);
+        System.out.println("3 " + b [3]);
+        System.out.println("4 " + b [4]);
+        System.out.println("5 " + b [5]);
+        System.out.println("6 " + b [6]);
+        System.out.println("7 " + b [7]);
+        System.out.println("8 " + b [8]);
+        System.out.println("9 " + b [9]);
+    }
+    
+    
+}
